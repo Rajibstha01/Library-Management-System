365f8d1b8272f21225b6e22c069bb28466ff4e1b
Rajib Shrestha <166205605+Rajibstha01@users.noreply.github.com>
2026-08-16 12:56:59 +0545

    Update config-page.js

--- Frontend/config-page.js
+++ Frontend/config-page.js
@@ -1,2 +1,2 @@
 var API_URL = "https://library-management-system-7ne7.onrender.com";
-window.API_BASE = "https://library-management-system-7ne7.onrender.com";
+window.API_BASE = "https://library-management-system-7ne7.onrender.com/api/v1";
